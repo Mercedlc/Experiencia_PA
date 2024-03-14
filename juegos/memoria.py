96e5f8d37d53db680f955c28dda1fe8e738fb559
mariasofiawahl <110851359+mariasofiawahl@users.noreply.github.com>
2024-03-14 14:54:08 +0000

    está listo el juego memoria

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,5 @@
+
+import random
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +7,25 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    memoria=[]
+    while len(memoria)<=5:
+        agregar = random.randint(0, 9)
+        memoria.append(agregar)
+    print("memoriza estos números",memoria)
+
+    respuesta=input('escribe los números que memorizaste sin coma, ejemplo: 12345  ')
+    '''respuesta=int(respuesta)'''
+    respuesta1=list(respuesta)
+    respuesta1 = [int(elemento) for elemento in respuesta1]
+
+    if memoria==respuesta1:
+        print("GANASTE")
+    else:
+        respuestan=input('Intenta de nuevo escribe los números que memorizaste sin coma, ejemplo: 12345  ')
+        '''respuesta=int(respuesta)'''
+        respuesta2=list(respuestan)
+        respuesta2 = [int(elemento) for elemento in respuesta2]
+        if memoria==respuesta2:
+            print("GANASTE")
+        else:
+            print("Perdiste")
